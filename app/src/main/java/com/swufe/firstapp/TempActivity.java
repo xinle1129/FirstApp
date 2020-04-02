779e9d1e4bc19c5d9af4c38a57db7dac8f07ad8b
xinle1129 <2537106249@qq.com>
2020-04-02 11:05:17 +0800

    birthday card

--- app/src/main/java/com/swufe/firstapp/TempActivity.java
+++ app/src/main/java/com/swufe/firstapp/TempActivity.java
@@ -1,11 +1,13 @@
 package com.swufe.firstapp;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TempActivity extends AppCompatActivity implements View.OnClickListener{//继承该接口用于监听按钮
     TextView out;//创建全局变量
@@ -22,9 +24,16 @@ public class TempActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(View v) {
                 //Log.i("main","onClick called....");//测试用
-                float f = Float.parseFloat(inp.getText().toString());//获取输入的华氏度的大小
-                float c = (float) ((f-32)/1.8);//将华氏度转换为摄氏度
-                out.setText("Celsius：  "+(float)Math.round(c*10)/10+"  ℃");//将摄氏度保留1位小数，并输出
+                String str = inp.getText().toString();
+                if(str.length() > 0){
+                    float f = Float.parseFloat(inp.getText().toString());//获取输入的华氏度的大小
+                    float c = (float) ((f-32)/1.8);//将华氏度转换为摄氏度
+                    out.setText("Celsius：  "+(float)Math.round(c*10)/10+"  ℃");//将摄氏度保留1位小数，并输出
+                }else{
+                    Toast.makeText(TempActivity.this, "请输入温度", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
     }
