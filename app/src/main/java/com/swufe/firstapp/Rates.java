d2bc3505c1e488f1bc5baf52cb12a3521c66d8c3
xinle1129 <2537106249@qq.com>
2020-04-09 11:04:32 +0800

    birthday card

--- app/src/main/java/com/swufe/firstapp/Rates.java
+++ app/src/main/java/com/swufe/firstapp/Rates.java
@@ -3,19 +3,22 @@ package com.swufe.firstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Rates extends AppCompatActivity {
     private final String TAG = "Rate";
-    private float dollarRate = 0.1f;
-    private float euroRate = 0.2f;
-    private float wonRate = 0.3f;
+    float dollarRate = 0.1f;
+    float euroRate = 0.2f;
+    float wonRate = 0.3f;
     EditText rmb;
     TextView show;
     @Override
@@ -43,7 +46,11 @@ public class Rates extends AppCompatActivity {
         }
     }
     public void openOne(View btn){
-        Intent config = new Intent(this,Config.class);
+        openConfig();
+    }
+
+    private void openConfig() {
+        Intent config = new Intent(this, Config.class);
         config.putExtra("dollar_rate_key",dollarRate);
         config.putExtra("euro_rate_key",euroRate);
         config.putExtra("won_rate_key",wonRate);
@@ -52,14 +59,33 @@ public class Rates extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.menu_set){
+            openConfig();;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         Log.i("woqu","cao");
-        if (requestCode == 1 && requestCode == 2) {
+        Log.i("woqu","requestcode"+requestCode);
+        Log.i("woqu","resultcode"+resultCode);
+        if (requestCode==1 && requestCode==2){
+            Log.i("woqu","cao1");
             Bundle bundle = data.getExtras();
+            Log.i("woqu","cao2");
             dollarRate = bundle.getFloat("key_dollar",0.1f);
             euroRate = bundle.getFloat("key_euro",0.1f);
             wonRate = bundle.getFloat("key_won",0.1f);
-            Log.i(TAG,"dollar: "+ dollarRate);
+            Log.i("woqu","cao3");
+            Log.i("tag","dollar: "+ dollarRate);
             Log.i(TAG,"euro: "+ euroRate);
             Log.i(TAG,"won: "+ wonRate);
         }
