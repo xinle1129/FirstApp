4c54419aa650091833e74633e905894a42017b66
xinle1129 <2537106249@qq.com>
2020-04-17 21:20:49 +0800

    html analysis screen rotary layout how to save data when rotary

--- app/src/main/java/com/swufe/firstapp/SecondActivity.java
+++ app/src/main/java/com/swufe/firstapp/SecondActivity.java
@@ -5,9 +5,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    final String TAG = "second";
     TextView score;
     TextView score2;
     @Override
@@ -16,6 +18,64 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         score = (TextView)findViewById(R.id.score1_second);
         score2 = (TextView)findViewById(R.id.score2_second);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {//保存数据0000
+        super.onSaveInstanceState(outState);
+        String scorea = (String) score.getText();
+        String scoreb = (String) score2.getText();
+
+        Log.i(TAG,"onSaveInstanceState:");
+        outState.putString("teama_score",scorea);
+        outState.putString("teamb_score",scoreb);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {//获取数据0000
+        super.onRestoreInstanceState(savedInstanceState);
+        String scorea = savedInstanceState.getString("teama_score");
+        String scoreb = savedInstanceState.getString("teamb_score");
+
+        Log.i(TAG,"onRestoreInstanceState:");
+        score.setText(scorea);
+        score2.setText(scoreb);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart:");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause:");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume:");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop:");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart:");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy:");
     }
 
     public void btnAdd1(View btn){
